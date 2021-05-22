589704e76071e78673859304bd895182f0f1aab0
Crowdbotics <team@crowdbotics.com>
2021-05-22 10:56:02 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/ash2_27240/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ash2_27240;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "ash2_27240";
+  }
+}
